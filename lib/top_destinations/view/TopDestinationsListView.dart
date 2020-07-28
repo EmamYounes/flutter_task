5d33b5e7d1236a10095e46f6c2ba6e2a011261c1
Emam <emamAhmed36@gmail.com>
2020-07-28 11:36:20 +0200

    add buildTextOnImage

--- lib/top_destinations/view/TopDestinationsListView.dart
+++ lib/top_destinations/view/TopDestinationsListView.dart
@@ -24,31 +24,78 @@ class _TopDestinationsListState extends State<TopDestinationsListView> {
         stream: _topDestinationsListBloc.topListSubject.stream,
         builder: (context, snapshot) {
           return snapshot.hasData
-              ? Container(
-                  height: 150,
-                  child: ListView.builder(
-                    shrinkWrap: true,
-                    scrollDirection: Axis.horizontal,
-                    itemCount: snapshot.data.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      DestinationModel model = snapshot.data[index];
-                      return Container(
-                        margin: EdgeInsets.all(10),
-                        child: ClipRRect(
-                          borderRadius: new BorderRadius.circular(10.0),
-                          child: Image(
-                            image: AssetImage(
-                              model.imageUrl,
-                            ),
-                            width: 150,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                )
+              ? buildItem(snapshot)
               : Center(child: CircularProgressIndicator());
         });
+  }
+
+  Container buildItem(AsyncSnapshot<List<DestinationModel>> snapshot) {
+    return Container(
+      height: 150,
+      child: ListView.builder(
+        shrinkWrap: true,
+        scrollDirection: Axis.horizontal,
+        itemCount: snapshot.data.length,
+        itemBuilder: (BuildContext context, int index) {
+          DestinationModel model = snapshot.data[index];
+          return Container(
+            margin: EdgeInsets.all(10),
+            child: Stack(
+              children: <Widget>[
+                buildImage(model),
+                buildTextOnImage(model),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Positioned buildTextOnImage(DestinationModel model) {
+    return Positioned(
+      bottom: 0,
+      child: Column(
+        children: <Widget>[
+          Text(
+            model.city,
+            style: TextStyle(
+                fontSize: 18,
+                color: Colors.white,
+                fontWeight: FontWeight.normal),
+          ),
+          Row(
+            children: <Widget>[
+              Icon(
+                Icons.call_made,
+                color: Colors.white70,
+                size: 10,
+              ),
+              Text(
+                model.country,
+                style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.white70,
+                    fontWeight: FontWeight.normal),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  ClipRRect buildImage(DestinationModel model) {
+    return ClipRRect(
+      borderRadius: new BorderRadius.circular(10.0),
+      child: Image(
+        image: AssetImage(
+          model.imageUrl,
+        ),
+        height: 150,
+        width: 150,
+        fit: BoxFit.cover,
+      ),
+    );
   }
 }
